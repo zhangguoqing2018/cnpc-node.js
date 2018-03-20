a338b53da31bed34850fc763b2ed2d27b77a2e0c
zhangguoqing <37327216+zhangguoqing2018@users.noreply.github.com>
2018-03-20 11:13:49 +0800

    Add files via upload

--- 1.js
+++ 1.js
@@ -1,6 +1,6 @@
 //美元转人民币
 const USDtoCNY = (USD) => {
-    const num = USD / 6.33;
+    const num = USD * 6.33;
     console.log(`USD美元(${USD}) Change into CNY人民币 Equal ${num.toFixed(2)}`);
 }
 //输入美元
@@ -10,7 +10,7 @@ USDtoCNY(USD);
 
 //人民币转美元
 const CNYtoUSD = (CNY) => {
-    const num = CNY * 6.33;
+    const num = CNY / 6.33;
     console.log('CNY人民币(' + CNY + ') Change into USD美元 Equal ' + num.toFixed(2));
 }
 
--- 3.js
+++ 3.js
@@ -3,19 +3,19 @@ const MoneytoCNY = (Style,Num) => {
     if(Num > 0){
         switch(Style) {
             case 'USD':
-                console.log('美元(' + Style + ') 数额('+ Num +') Change into 人民币CNY Equal '+ (Num / 6.3211).toFixed(2));
+                console.log('美元(' + Style + ') 数额('+ Num +') Change into 人民币CNY Equal '+ (Num * 6.3211).toFixed(2));
                 break;
             case 'JPY':
-                console.log(`日元(${Style}) 数额(${Num}) Change into 人民币CNY Equal ${(Num / 0.0594).toFixed(2)}`);
+                console.log(`日元(${Style}) 数额(${Num}) Change into 人民币CNY Equal ${(Num * 0.0594).toFixed(2)}`);
                 break;
             case 'EUR':
-                console.log('欧元(' + Style + ') 数额('+ Num +') Change into 人民币CNY Equal '+ (Num / 7.7950).toFixed(2));
+                console.log('欧元(' + Style + ') 数额('+ Num +') Change into 人民币CNY Equal '+ (Num * 7.7950).toFixed(2));
                 break;
             case 'RUB':
-                console.log(`卢布(${Style}) 数额(${Num}) Change into 人民币CNY Equal ${(Num / 0.1117).toFixed(2)}`);
+                console.log(`卢布(${Style}) 数额(${Num}) Change into 人民币CNY Equal ${(Num * 0.1117).toFixed(2)}`);
                 break;
             case 'CHF':
-                console.log('法郎(' + Style + ') 数额('+ Num +') Change into 人民币CNY Equal '+ (Num / 6.6181).toFixed(2));
+                console.log('法郎(' + Style + ') 数额('+ Num +') Change into 人民币CNY Equal '+ (Num * 6.6181).toFixed(2));
                 break;
             default:
                 console.log('请检查录入的货币类型是否正确！！');
@@ -27,7 +27,7 @@ const MoneytoCNY = (Style,Num) => {
 }
 
 //输入货币类型和数值
-const Style = 'USD';
+const Style = 'JPY';
 const Num = 50;
 
 MoneytoCNY(Style,Num);
